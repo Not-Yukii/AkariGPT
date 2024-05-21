1db165736657e9ac385ce8e1dec1e0510fcb531d
Not-Yukii <96383296+Not-Yukii@users.noreply.github.com>
2024-05-21 18:07:15 +0200

    last_commit

--- lib/screens/connexion_screen/connexion_screen.dart
+++ lib/screens/connexion_screen/connexion_screen.dart
@@ -174,7 +174,7 @@ class ConnexionScreen extends StatelessWidget {
 
   /// Navigates to the mondesContainerScreen when the action is triggered.
   void onTapConfirmer(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.mondesScreen);
+    Navigator.pushNamed(context, AppRoutes.jeuFeuScreen);
   }
 
   /// Navigates to the inscriptionScreen when the action is triggered.
